6f1e7bb9b29a891fb7d0585db6249f9140e1fc9f
enes2 <ekaya2110@posta.pau.edu.tr>
2023-12-13 07:10:22 +0300

    deneme

---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func DNS(service string) ([]string, error) {
+	ipaddrs, err := net.LookupIP(service)
+	if err != nil {
+		return nil, err
+	}
+
+	var ipStrList []string
+	for _, ip := range ipaddrs {
+		ipStr := ip.String()
+		ipStrList = append(ipStrList, ipStr)
+		fmt.Println(ipStr)
+	}
+
+	return ipStrList, nil
+}
+
+func main() {
+	checker := gin.Default()
+	htmlTemplate := template.Must(template.New("index").Parse(`
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<title>DNS Checker</title>
+	</head>
+	<body>
+		<h1>DNS Checker</h1>
+		<form action="/" method="get">
+			<label for="service">Servis Adı:</label>
+			<input type="text" name="service" required>
+			<button type="submit">Sorgula</button>
+		</form>
+		{{ if .Values }}
+			<h2>IP Adresleri:</h2>
+			<ul>
+				{{ range .Values }}
+					<li>{{ . }}</li>
+				{{ end }}
+			</ul>
+		{{ end }}
+	</body>
+	</html>
+	`))
+
+	checker.GET("/", func(c *gin.Context) {
+		service := c.Query("service")
+		if service == "" {
+			c.HTML(http.StatusBadRequest, "index", gin.H{"error": "Servis adı boş olamaz"})
+			return
+		}
+
+		values, err := DNS(service)
+		if err != nil {
+			c.HTML(http.StatusInternalServerError, "index", gin.H{"error": fmt.Sprintf("IP adresi bulunamadı: %v", err)})
+			return
+		}
+
+		c.HTML(http.StatusOK, "index", gin.H{
+			"Values": values,
+		})
+	})
+
+	checker.SetHTMLTemplate(htmlTemplate)
+	err := checker.Run(":8080")
+	if err != nil {
+		return
+	}
+}
