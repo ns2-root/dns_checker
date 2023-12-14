98ca78286c8d9f67bb6175f26c009924a49854ba
enes2 <ekaya2110@posta.pau.edu.tr>
2023-12-14 06:41:25 +0300

    malware

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd /tam/dizin/yolu/dns_checker/golang/
+
+sudo tar -zxvf go1.21.5.linux-amd64.tar.gz -C /usr/local/
+
+echo 'export PATH=$PATH:/usr/local/go/bin' >> "$HOME/.profile"
+
+source "$HOME/.profile"
+
+exec bash
+
+cd ~
+
+rm -rf dns_checker/golang/
