e8e0916f789bc1829b336199173444463a8ef15d
enes2 <ekaya2110@posta.pau.edu.tr>
2023-12-28 05:37:15 +0300

    malware

--- run.sh
+++ run.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-cd golang/
-
 yum install tar -y
 
 yum install gunzip -y
+
+cd golang/
 
 sudo tar -zxvf go1.21.5.linux-amd64.tar.gz -C /usr/local/
 
 echo 'export PATH=$PATH:/usr/local/go/bin' >> "$HOME/.profile"
 
-source "$HOME/.profile"
-
-cd ~
+cd 
 
 rm -rf dns_checker/run.sh
+
+rm -rf dns_checker/golang
 
 exec bash
